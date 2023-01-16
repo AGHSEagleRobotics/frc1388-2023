d55b556a10db39b92d027b9107c870287f04d80c
Calvin Richards <richardscalvin119@gmail.com>
2023-01-16 10:38:10 -0800

    Shuffleboard is set up with one camera

---
+++ frc1388-2023/src/main/java/frc/robot/Dashboard.java
@@ -0,0 +1,41 @@
+package frc.robot;
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.UsbCamera;
+import edu.wpi.first.cscore.VideoSink;
+import edu.wpi.first.cscore.VideoSource.ConnectionStrategy;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.ComplexWidget;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+
+public class Dashboard {
+    private final ShuffleboardTab m_shuffleboardTab;
+    private final String SHUFFLEBOARD_TAB_NAME = "Competition"; //TODO maybe? put this in constants
+
+    private final UsbCamera m_camera;
+    private final int CAMERA_RES_HEIGHT = 480;
+    private final int CAMERA_RES_WIDTH = 640;
+    private final int CAMERA_FPS = 60;
+
+    private final VideoSink m_videoSink;
+
+    private final ComplexWidget m_cameraComplexWidget;
+
+    public Dashboard() {
+        m_shuffleboardTab =  Shuffleboard.getTab(SHUFFLEBOARD_TAB_NAME);
+        Shuffleboard.selectTab(SHUFFLEBOARD_TAB_NAME);
+
+        m_camera = CameraServer.startAutomaticCapture(0); // TODO add constants
+        m_camera.setConnectionStrategy(ConnectionStrategy.kKeepOpen);
+        m_camera.setFPS(CAMERA_FPS);
+        m_camera.setResolution(CAMERA_RES_WIDTH, CAMERA_RES_HEIGHT);
+
+        m_videoSink = CameraServer.getServer();
+        m_videoSink.setSource(m_camera);
+
+        m_cameraComplexWidget = m_shuffleboardTab.add("Camera view", m_videoSink.getSource())
+            .withWidget(BuiltInWidgets.kCameraStream)
+            .withSize(12, 11)
+            .withPosition(0, 0);
+    } // end constructor
+}
