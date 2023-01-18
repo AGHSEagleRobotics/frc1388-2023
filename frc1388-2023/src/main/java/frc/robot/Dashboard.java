f638d7856600ca444c39723b908e5d8a86bda071
Calvin Richards <richardscalvin119@Gmail.com>
2023-01-17 19:46:59 -0800

    Added primary and secondary camera to dash  board

--- frc1388-2023/src/main/java/frc/robot/Dashboard.java
+++ frc1388-2023/src/main/java/frc/robot/Dashboard.java
@@ -1,42 +1,64 @@
 package frc.robot;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.cscore.UsbCamera;
+import edu.wpi.first.cscore.VideoMode;
 import edu.wpi.first.cscore.VideoSink;
 import edu.wpi.first.cscore.VideoSource.ConnectionStrategy;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.ComplexWidget;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.shuffleboard.WidgetType;
 
 public class Dashboard {
     private final ShuffleboardTab m_shuffleboardTab;
     private final String SHUFFLEBOARD_TAB_NAME = "Competition"; //TODO maybe? put this in constants
 
-    private final UsbCamera m_camera;
-    private final int CAMERA_RES_HEIGHT = 480;
-    private final int CAMERA_RES_WIDTH = 640;
-    private final int CAMERA_FPS = 60;
+    private final UsbCamera m_cameraLarge;
+    private final int CAMERA_LARGE_RES_HEIGHT = 480;
+    private final int CAMERA_LARGE_RES_WIDTH = 640;
+    private final int CAMERA_LARGE_FPS = 60;
 
-    private final VideoSink m_videoSink;
+    private final UsbCamera m_cameraSmall;
+    private final int CAMERA_SMALL_RES_HEIGHT = 120;
+    private final int CAMERA_SMALL_RES_WIDTH = 480;
+    private final int CAMERA_SMALL_FPS = 10;
 
-    private final ComplexWidget m_cameraComplexWidget;
+    private final VideoSink m_videoSinkLargeCamera;
+    private final VideoSink m_videoSinkSmallCamera;
 
+    private final ComplexWidget m_largeCameraComplexWidget;
+    private final ComplexWidget m_smallCameraComplexWidget;
+    
     public Dashboard() {
-        System.out.println("\n\n\n\n\n\n\n\ndashboard\n\n\n\n\n\n\n\n\n");
+        
+        System.out.println("**********************************\n\n\n\n\n\n\n\ndashboard\n\n\n\n\n\n\n\n\n****************************************");
         m_shuffleboardTab =  Shuffleboard.getTab(SHUFFLEBOARD_TAB_NAME);
         Shuffleboard.selectTab(SHUFFLEBOARD_TAB_NAME);
 
-        m_camera = CameraServer.startAutomaticCapture(0); // TODO add constants
-        m_camera.setConnectionStrategy(ConnectionStrategy.kKeepOpen);
-        m_camera.setFPS(CAMERA_FPS);
-        m_camera.setResolution(CAMERA_RES_WIDTH, CAMERA_RES_HEIGHT);
+        m_cameraLarge = CameraServer.startAutomaticCapture(0); // TODO add constants
+        m_cameraLarge.setConnectionStrategy(ConnectionStrategy.kKeepOpen);
+        m_cameraLarge.setFPS(CAMERA_LARGE_FPS);
+        m_cameraLarge.setResolution(CAMERA_LARGE_RES_WIDTH, CAMERA_LARGE_RES_HEIGHT);
+        m_videoSinkLargeCamera = CameraServer.getServer();
+        m_videoSinkLargeCamera.setSource(m_cameraLarge);
 
-        m_videoSink = CameraServer.getServer();
-        m_videoSink.setSource(m_camera);
+        m_cameraSmall = CameraServer.startAutomaticCapture(1); // TODO add constants
+        m_cameraSmall.setConnectionStrategy(ConnectionStrategy.kKeepOpen);
+        m_cameraSmall.setFPS(CAMERA_SMALL_FPS);
+        m_cameraSmall.setResolution(CAMERA_SMALL_RES_WIDTH, CAMERA_SMALL_RES_HEIGHT);
+        m_videoSinkSmallCamera = CameraServer.getServer();
+        m_videoSinkSmallCamera.setSource(m_cameraSmall);
 
-        m_cameraComplexWidget = m_shuffleboardTab.add("Camera view", m_videoSink.getSource())
+
+        m_largeCameraComplexWidget = m_shuffleboardTab.add("primary camera view", m_videoSinkLargeCamera.getSource())
             .withWidget(BuiltInWidgets.kCameraStream)
             .withSize(15, 14)
             .withPosition(0, 0);
+
+        m_smallCameraComplexWidget = m_shuffleboardTab.add("secondary camera view", m_videoSinkLargeCamera.getSource())
+            .withWidget(BuiltInWidgets.kCameraStream)
+            .withSize(5, 5)
+            .withPosition(16, 0);
     } // end constructor
 }
