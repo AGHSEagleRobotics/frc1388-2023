8656ee332052420fc5b609e34342e4bfacda4583
Mason Gomez <disiznotkool@gmail.com>
2023-01-28 10:59:53 -0800

    Created 2 separate subsystems for the separate IMUs the 16448 and the 16470

---
+++ frc1388-2023/src/main/java/frc/robot/subsystems/IMUSubsystem16470.java
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class IMUSubsystem16470 extends SubsystemBase {
+  /** Creates a new IMUSubsystem16470. */
+  public IMUSubsystem16470() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
