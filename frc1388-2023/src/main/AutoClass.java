d4c54e1c4293cf1988971efa1fce3cb489e39cf9
Isabelle Bowdey <Isabellebowt@gmail.com>
2023-01-30 18:43:17 -0800

    Deleted AutoClass + commented auto ideas in a doc

---
+++ frc1388-2023/src/main/AutoClass.java
@@ -0,0 +1,27 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package AutoClass;
+
+/** Add your docs here. */
+public class AutoClass {
+    //Step by step process of B auto to start
+
+    //extend claw
+    //set down cone on 3rd row, middle
+    //drive forward 25%, slow, go down station
+    //pause at bottom
+    //pick up game piece if time is left
+    //climb onto station
+
+    //Step by step A
+
+    //extent claw
+    //set down cone on 3rd level node in corner
+    //straight until close to game piece
+    //pick up
+    //straight back
+    //score on 2nd top piece
+    //head back to pick up another OR head to pickup zone
+}
