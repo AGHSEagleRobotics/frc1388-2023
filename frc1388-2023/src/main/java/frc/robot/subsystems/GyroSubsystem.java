ae08225e94a2bd9c34470b51138f3ddd4f0687be
Calvin Richards <richardscalvin119@gmail.com>
2023-03-05 13:20:28 -0800

    fixing gyros

--- frc1388-2023/src/main/java/frc/robot/subsystems/GyroSubsystem.java
+++ frc1388-2023/src/main/java/frc/robot/subsystems/GyroSubsystem.java
@@ -62,7 +62,8 @@ public class GyroSubsystem extends SubsystemBase {
 
       case ADIS16470Multi:
         xAngle = -m_gyro16470Multi.getGyroAngleX(); // Lint
-//        xAngle = m_gyro16470Multi.getGyroAngleY(); // RoboRio
+        //  xAngle = m_gyro16470Multi.getGyroAngleY(); // RoboRio
+        xAngle = m_gyro16470Multi.getGyroAngleY(); // 2023
         break;
 
       default:
@@ -75,7 +76,7 @@ public class GyroSubsystem extends SubsystemBase {
   public void resetXAngle(){
     switch (m_gyroType){
       case ADIS16470Multi:
-        m_gyro16470Multi.setGyroAngleX(0);
+        m_gyro16470Multi.setGyroAngleY(0);
         break;
 
       default:
@@ -113,7 +114,7 @@ public class GyroSubsystem extends SubsystemBase {
   public void resetYAngle(){
     switch (m_gyroType){
       case ADIS16470Multi:
-        m_gyro16470Multi.setGyroAngleY(0);
+        m_gyro16470Multi.setGyroAngleX(0);
         break;
 
       default:
