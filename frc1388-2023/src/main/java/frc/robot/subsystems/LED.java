23eabefdfa560b1b40574e1b5970d36cde767c52
Mason Gomez <disiznotkool@gmail.com>
2023-03-22 18:59:58 -0700

    removed stuff that we are not using in this year's leds

--- frc1388-2023/src/main/java/frc/robot/subsystems/LED.java
+++ frc1388-2023/src/main/java/frc/robot/subsystems/LED.java
@@ -4,76 +4,27 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.LEDConstants;
 
 public class LED extends SubsystemBase {
 
-  private PWMSparkMax m_LedArms;
-  private PWMSparkMax m_LedBody;
-  private boolean m_notAtComp = true;
-  private boolean m_shooting = false;
-  private boolean m_isBlue; // set in perodic
-
+  private PWMSparkMax m_leds;
+  private boolean maxArm;
+  
   /** Creates a new LED. */
-  public LED(PWMSparkMax bodyLeds, PWMSparkMax armLeds) {
-    m_LedBody = bodyLeds;
-      m_LedBody.setSafetyEnabled(false);
-    m_LedArms = armLeds;
-      m_LedArms.setSafetyEnabled(false);
+  public LED(PWMSparkMax leds ) {
+    m_leds = leds;
+      m_leds.setSafetyEnabled(false);
   } // end constructor
 
 
-  public void ledShoot() {
-    m_shooting = true;
-  }
-
-  public void ledNormal() {
-    m_shooting = false;
-  }
 
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
     
-    // m_shooting = true;
-    m_isBlue = (DriverStation.getAlliance() == DriverStation.Alliance.Blue);
-
-    if ((DriverStation.isFMSAttached() || m_notAtComp)) {
-      if (m_isBlue) {
-
-        m_LedArms.set(LEDConstants.BLUE_LARSON);
-        // log.info("blue arms");
-
-        if (m_shooting) {
-          m_LedBody.set(LEDConstants.BLUE_FLASH);
-        // log.info("blue shoot");
-        } else {
-          m_LedBody.set(LEDConstants.BLUE_SOLID);
-        // log.info("blue solid");
-        }
-
-      } else {
-
-        m_LedArms.set(LEDConstants.RED_LARSON);
-        // log.info("red arms");
-
-        if (m_shooting) {
-          m_LedBody.set(LEDConstants.RED_FLASH);
-        // log.info("red shoot");
-        } else { 
-          m_LedBody.set(LEDConstants.RED_SOLID);
-        // log.info("red solid");
-        }
-
-      }
-    } // end if
-
-    // log.info("\n\n\n" + m_shooting + "\n\n\n");
-  
   }
 }
